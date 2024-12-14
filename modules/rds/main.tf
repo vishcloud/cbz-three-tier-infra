0c8302261f4b2c6232e0109a6e0f779bc466e6a2
skalsait <skalsait@dyl.com>
2024-12-14 11:53:13 +0530

    --- Auto Git Commit ---

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -43,7 +43,6 @@ resource "aws_db_instance" "cbz_db_instance" {
   engine               = "mysql"
   engine_version       = "8.0"
   instance_class       = "db.t3.micro" # Free-tier eligible instance type
-  name                 = "cbzdb"
   username             = "admin"
   password             = "Redhat123"
   parameter_group_name = "default.mysql8.0"

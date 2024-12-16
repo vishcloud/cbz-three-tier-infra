ba34afb75f299960945bd3caafcf140939dfbe92
skalsait <skalsait@dyl.com>
2024-12-16 15:44:09 +0530

    --- Auto Git Commit ---

--- modules/eks/output.tf
+++ modules/eks/output.tf
@@ -1,5 +1,5 @@
 # Output the kubeconfig
-output "kubeconfig" {
-  value = aws_eks_cluster.cbz_cluster.identity[0].oidc.issuer
-  description = "Run 'aws eks update-kubeconfig --name cbz-cluster' to configure kubeconfig"
-}
+# output "kubeconfig" {
+#   value = aws_eks_cluster.cbz_cluster.identity[0].oidc.issuer
+#   description = "Run 'aws eks update-kubeconfig --name cbz-cluster' to configure kubeconfig"
+# }
